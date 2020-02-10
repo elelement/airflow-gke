8f07d1d6547b28d5320ab37968bc83e79082e86c
JMC <josele7@gmail.com>
2020-02-10 11:46:02 +0100

    Changed network and subnet parameters in order to be more specific

--- infrastructure.tf
+++ infrastructure.tf
@@ -1,4 +1,6 @@
 variable "project" {}
+variable "network_id" {}
+variable "subnet_id" {}
 
 variable "postgres_user" {
   default = "airflow"
@@ -23,7 +25,7 @@ provider "google" {
 
 data "google_compute_network" "default" {
   project = "${var.project}"
-  name    = "${var.network}"
+  name    = "${var.network_id}"
 }
 
 resource "google_compute_global_address" "airflow-static-ip" {
@@ -37,8 +39,8 @@ resource "google_compute_disk" "airflow-redis-disk" {
   zone  = "${var.zone}"
 }
 
-resource "google_sql_database_instance" "airflow-db" {
-  name = "airflow-db"
+resource "google_sql_database_instance" "airflow-db-2" {
+  name = "airflow-db-2"
   database_version = "POSTGRES_11"
   region = "${var.region}"
   settings {
@@ -48,21 +50,21 @@ resource "google_sql_database_instance" "airflow-db" {
 
 resource "google_sql_database" "airflow-schema" {
   name = "airflow"
-  instance = "${google_sql_database_instance.airflow-db.name}"
+  instance = "${google_sql_database_instance.airflow-db-2.name}"
 }
 
 resource "google_sql_user" "proxyuser" {
   name = "${var.postgres_user}"
   password = "${var.postgres_pw}"
-  instance = "${google_sql_database_instance.airflow-db.name}"
+  instance = "${google_sql_database_instance.airflow-db-2.name}"
   host = "cloudsqlproxy~%"
 }
 
 resource "google_container_cluster" "airflow-cluster" {
   name = "airflow-cluster"
   location = "${var.zone}"
-  network = "${var.network}"
-  subnetwork = "${var.subnet}"
+  network = "${var.network_id}"
+  subnetwork = "${var.subnet_id}"
   initial_node_count = "1"
   node_config {
     machine_type = "n1-standard-4"
